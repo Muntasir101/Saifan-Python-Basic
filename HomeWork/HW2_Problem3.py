bec7d7664d60760b32fff3814187c5d3cfe57ddc
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-07-07 21:30:47 +0600

    HW

--- HomeWork/HW2_Problem3.py
+++ HomeWork/HW2_Problem3.py
@@ -2,8 +2,9 @@ number1 = int(input("Enter the First Number: "))
 number2 = int(input("Enter the Second Number: "))
 number3 = int(input("Enter the Third Number: "))
 number4 = int(input("Enter the Fourth Number: "))
+number5 = int(input("Enter the Fifth Number: "))
 
-largest = max(number1, number2, number3, number4)
+largest = max(number1, number2, number3, number4, number5)
 smallest = min(number1, number2, number3, number4)
 
 print(f"Largest Number is: {largest}")
